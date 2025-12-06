a25a9ca3013efd0c70d556fa96fd5defbeac6b18
alphaomega4348 <alphaomega4348@gmail.com>
2025-12-06 22:46:22 +0530

    final keyword

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,41 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Car audi= new Car(3901.1f);
-        audi.engine="Electric Engine";
-        audi.numOfSeats=4;
-        System.out.println("After creation");
+//        Car audi= new Car(3901.1f);
+//        audi.engine="Electric Engine";
+//        audi.numOfSeats=4;
+//        System.out.println("After creation");
+//
+//        System.out.println(audi.engine+" "+audi.numOfSeats+" "+audi.price);
+       final A shivam=new A("shivam");
+        shivam.name="kumar"; //allowed
+        //When a non-primitve is final you can't reassign it.
+        //shivam=new A("hero"); //not allowed
 
-        System.out.println(audi.engine+" "+audi.numOfSeats+" "+audi.price);
     }
+
 
 }
 
 
-class Car{
-    String engine;
-    Float price;
-    int numOfSeats;
+//class Car{
+//    String engine;
+//    Float price;
+//    int numOfSeats;
+//
+//    Car(Float price){
+//        this.price=price;
+//        System.out.println("Instantiated");
+//    }
+//}
 
-    Car(Float price){
-        this.price=price;
-        System.out.println("Instantiated");
+class A{
+
+    final int num=20;
+    String name;
+
+    public A(String name){
+        this.name=name;
     }
+
 }
