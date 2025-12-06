d145f37a1eb88857ec79bfd9f9e1f38f738cb5f3
alphaomega4348 <alphaomega4348@gmail.com>
2025-12-06 23:57:15 +0530

    static class

--- src/staticExample/Main.java
+++ src/staticExample/Main.java
@@ -2,10 +2,21 @@ package staticExample;
 
 public class Main {
     public static void main(String[] args) {
-        Human shivam= new Human(21,"Shivam",1000,false);
-        Human amar= new Human(22,"Amar",15000,true);
-        System.out.println(shivam.population);
-        System.out.println(amar.population);
+//        Human shivam= new Human(21,"Shivam",1000,false);
+//        Human amar= new Human(22,"Amar",15000,true);
+//        System.out.println(shivam.population);
+//        System.out.println(amar.population);
 
+
+
+    }
+    //static method doesn't depend on objects.
+    static void fun(){
+        greeting(); //can't call a non-static method from static method
+    }
+
+    //If something is not static then it belongs to object.
+    void greeting(){
+        System.out.println("Hello");
     }
 }
